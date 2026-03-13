bfd85df6cb7189ec6e16cb78ad7d0e761590e73a
TMUNDIN <tobias.mundinger@mercedes-benz.com>
2026-03-13 14:18:33 +0100

    Add elarion.saboti.de host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 5173
+    port: 5173,
+    allowedHosts: ['elarion.saboti.de']
   },
   esbuild: {
     loader: 'jsx',
